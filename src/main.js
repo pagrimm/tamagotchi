c998695e914a37483736b8a9640fefb0673fd52f
Tyson Lackey <lackeyt90@gmail.com>
2020-06-30 10:51:45 -0700

    update API call ui
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,5 +5,22 @@ import $ from "jquery";
 
 $(document).ready(function() {
   //User Interface
+  addGiphy();
 });
 
+
+
+function addGiphy() {
+  let request = new XMLHttpRequest();
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=kitten&rating=g`
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      $(".showRandomKitten").first().append(`<img src="${response.data.images.downsized_large.url}">`);
+    }
+  }
+
+  request.open("GET", url, true);
+  request.send();
+}
